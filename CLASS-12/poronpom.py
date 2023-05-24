d77c665c2d532b069920c9884ba6ad8c4b2a5d71
Cometa46 <134543330+Cometa46@users.noreply.github.com>
2023-05-24 14:53:33 -0400

    Archivo validado

--- CLASS-12/poronpom.py
+++ CLASS-12/poronpom.py
@@ -3,8 +3,13 @@ from queue import PriorityQueue
 
 print("datos personales")
 print(" ---------------- \n")
-Vnom = input("Ingrese nombre")
-Vedad = input("Ingrese su edad")
+Vnom = input("Ingrese nombre ")
+while True:
+    try:
+        Vedad = int(input("Ingrese su edad "))
+        break
+    except:
+        print("Valor no corresponde")
 print(" ---------------- \n")
 print(f"Su nombre es: {Vnom} \n su edad es: {Vedad}")
 
