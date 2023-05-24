a0c49c40e794e5ebcea59c11fd898f4086cec5d9
Cometa46 <134543330+Cometa46@users.noreply.github.com>
2023-05-24 14:34:09 -0400

    Código sin validar

---
+++ CLASS-12/poronpom.py
@@ -0,0 +1,11 @@
+from queue import PriorityQueue
+
+
+print("datos personales")
+print(" ---------------- \n")
+Vnom = input("Ingrese nombre")
+Vedad = input("Ingrese su edad")
+print(" ---------------- \n")
+print(f"Su nombre es: {Vnom} \n su edad es: {Vedad}")
+
+print("Programa finaizado")
